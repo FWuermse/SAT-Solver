5a5877e28df57365f4d2cb8134b06462e8793529
Florian Wuermseer <wuermsee@hm.edu>
2023-12-02 00:06:43 +0100

    feat: implement backtracking

--- src/dpll.rs
+++ src/dpll.rs
@@ -70,6 +70,7 @@ fn solve(input: Vec<Vec<i32>>) -> DIMACSOutput {
             );
             match literal.signum() {
                 // TODO: Is it neccessary to check whether a variable occurs twice in same pol. in a clause?
+                // When not checking clauses like (a \/ -a \/ ...) would not be taken into account.
                 1 => pos_occ
                     .entry(lit)
                     .and_modify(|clauses: &mut Vec<usize>| clauses.push(c))
@@ -124,7 +125,25 @@ fn solve(input: Vec<Vec<i32>>) -> DIMACSOutput {
 
         // * if conflict detected
         if unsat {
-            backtrack();
+            let unsat = backtrack(
+                &mut clauses,
+                &mut assignment_stack,
+                &mut var_value,
+                &mut unit_queue,
+                &mut pos_occ,
+                &mut neg_occ,
+            );
+            if unsat {
+                return DIMACSOutput::Unsat;
+            }
+            let _ = unit_prop(
+                &mut unit_queue,
+                &mut clauses,
+                &mut assignment_stack,
+                &mut var_value,
+                &mut pos_occ,
+                &mut neg_occ,
+            );
         }
 
         // * if all clauses satisfied
@@ -134,8 +153,38 @@ fn solve(input: Vec<Vec<i32>>) -> DIMACSOutput {
     }
 }
 
-fn backtrack() {
-    todo!()
+fn backtrack(
+    clauses: &mut HashMap<usize, Clause>,
+    assignment_stack: &mut Vec<Assignment>,
+    var_value: &mut HashMap<i32, Variable>,
+    unit_queue: &mut VecDeque<usize>,
+    pos_occ: &mut HashMap<i32, Vec<usize>>,
+    neg_occ: &mut HashMap<i32, Vec<usize>>,
+) -> bool {
+    let mut last_step = assignment_stack.pop();
+    while last_step.as_ref().is_some_and(|step| step.forced) {
+        unset_var(last_step.unwrap().var, clauses, var_value, pos_occ, neg_occ);
+        last_step = assignment_stack.pop();
+    }
+    if assignment_stack.is_empty() {
+        return true;
+    }
+    let var = last_step.as_ref().unwrap().var;
+    let val = last_step.unwrap().value;
+    unset_var(var, clauses, var_value, pos_occ, neg_occ);
+    unit_queue.clear();
+    set_var(
+        var,
+        true,
+        !val,
+        unit_queue,
+        clauses,
+        assignment_stack,
+        var_value,
+        pos_occ,
+        neg_occ,
+    );
+    false
 }
 
 // TODO: @Laura hier können die Heuristiken rein. Gerne auch mit enum flag welche an/aus sind.
@@ -198,15 +247,27 @@ fn set_var(
     assignment_stack.push(Assignment { var, value, forced });
     let mut var_value = var_value.get_mut(&var.abs()).unwrap();
     var_value.is_free = false;
-    var_value.value = value;
+    // -1 true => 1 false
+    // -1 false => 1 true
+    // 1 true => 1 true
+    // 1 false => 1 false
+    // <=> var.is_pos == value
+    var_value.value = value == var.is_positive();
     // mark all clauses with pos_occ as sat
-    pos_occ.get(&var).unwrap().iter().for_each(|c| {
+    // -1 true => pos 1 sat
+    // -1 false => neg 1 sat
+    // 1 true => pos 1 sat
+    // 1 false => neg 1 sat
+    let (mark_sat, mark_unsat) = match var.is_positive() & value {
+        true => (pos_occ, neg_occ),
+        false => (neg_occ, pos_occ),
+    };
+    mark_sat.get(&var.abs()).unwrap().iter().for_each(|c: &usize| {
         clauses.entry(*c).and_modify(|sat_clause| {
             sat_clause.sat_by_var = var;
-            sat_clause.unassign_vars = sat_clause.unassign_vars - 1;
         });
     });
-    for c in neg_occ.get(&var).unwrap() {
+    for c in mark_unsat.get(&var.abs()).unwrap() {
         let unsat_clause = clauses.get_mut(c).unwrap();
         unsat_clause.unassign_vars = unsat_clause.unassign_vars - 1;
         match unsat_clause.unassign_vars {
@@ -218,13 +279,44 @@ fn set_var(
     false
 }
 
+fn unset_var(
+    var: BVar,
+    clauses: &mut HashMap<usize, Clause>,
+    var_value: &mut HashMap<i32, Variable>,
+    pos_occ: &mut HashMap<i32, Vec<usize>>,
+    neg_occ: &mut HashMap<i32, Vec<usize>>,
+) {
+    let mut var_value = var_value.get_mut(&var.abs()).unwrap();
+    var_value.is_free = true;
+    // Value actually doesn't matter
+    var_value.value = false;
+    let (mark_sat, mark_unsat) = match var.is_positive() & var_value.value {
+        true => (pos_occ, neg_occ),
+        false => (neg_occ, pos_occ),
+    };
+    mark_sat.get(&var).unwrap().iter().for_each(|c| {
+        clauses.entry(*c).and_modify(|sat_clause| {
+            if sat_clause.sat_by_var == var {
+                sat_clause.sat_by_var = 0;
+            }
+        });
+    });
+    for c in mark_unsat.get(&var).unwrap() {
+        let unsat_clause = clauses.get_mut(c).unwrap();
+        unsat_clause.unassign_vars = unsat_clause.unassign_vars - 1;
+    }
+}
+
 #[test]
 fn should_solve_sat() {
-    solve(vec![vec![1, -2, 3], vec![-1, 2], vec![-1, -2, -3]]);
+    let res = solve(vec![vec![1, -2, 3], vec![-1, 2], vec![-1, -2, -3]]);
+    if let DIMACSOutput::Unsat = res {
+        panic!("Was UNSAT but expected SAT.")
+    }
 }
 
 fn should_solve_unsat() {
-    solve(vec![
+    let res = solve(vec![
         vec![1, -2, 3],
         vec![-1, 2],
         vec![-1, -2, -3],
@@ -232,4 +324,7 @@ fn should_solve_unsat() {
         vec![2],
         vec![3],
     ]);
+    if let DIMACSOutput::Sat(_) = res {
+        panic!("Was SAT but expected UNSAT.")
+    }
 }
