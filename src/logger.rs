09b39c7cdb26aab6c53644b352165071747dc5ce
NoelHuibers <noel@huibers.io>
2024-03-11 20:09:14 +0100

    Adding logging
    Former-commit-id: 8be6f644dca1cb7f16cfd08704eba795926c3362

---
+++ src/logger.rs
@@ -0,0 +1,24 @@
+use std::io::{BufWriter, Write};
+
+struct ProofLogger {
+    writer: BufWriter<std::fs::File>,
+}
+
+impl ProofLogger {
+    pub fn new(path: &str) -> std::io::Result<Self> {
+        let file = std::fs::File::create(path)?;
+        let writer = BufWriter::new(file);
+        Ok(ProofLogger { writer })
+    }
+
+    pub fn log_clause(&mut self, clause: &[i32], operation: char) -> std::io::Result<()> {
+        // Prefix with 'a' for add or 'd' for delete
+        write!(self.file, "{} ", operation)?;
+        // Write the clause
+        for &lit in clause {
+            write!(self.file, "{} ", lit)?;
+        }
+        // End with 0
+        writeln!(self.file, "0")
+    }
+}
